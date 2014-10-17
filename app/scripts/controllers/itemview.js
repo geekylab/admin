b3099ece6099f70db032250ccfabe9591daada0f
johna1203 <johnathan@kodokuman.com>
2014-10-17 01:18:22 -0300

    item crud

--- app/scripts/controllers/itemview.js
+++ app/scripts/controllers/itemview.js
@@ -10,7 +10,7 @@
 angular.module('clientApp')
     .controller('ItemviewCtrl', function ($scope, Items, $location, $routeParams, alertService) {
 
-        if ($routeParams.id) {
+        if ($routeParams.id != -1) {
             $scope.item = Items.get({id: $routeParams.id});
         } else {
             $scope.item = new Items();
@@ -36,7 +36,7 @@ angular.module('clientApp')
                 });
             }
 
-            if ($routeParams.id) {
+            if ($scope.item._id) {
                 Items.update($scope.item, success, failure);
             } else {
                 Items.save($scope.item, success, failure);
@@ -44,7 +44,26 @@ angular.module('clientApp')
         };
 
         $scope.delete = function () {
-            alert('delete');
+
+            function success(response) {
+                alertService.add('success', '削除されました。');
+                $location.path("/item");
+            }
+
+            function failure(response) {
+                _.each(response.data, function (errors, key) {
+                    if (errors.length > 0) {
+                        _.each(errors, function (e) {
+                            $scope.form[key].$dirty = true;
+                            $scope.form[key].$setValidity(e, false);
+                        });
+                    }
+                });
+            }
+
+            if ($scope.item._id) {
+                Items.delete({id: $scope.item._id}, success, failure);
+            }
         };
 
     });
--- server/routes/api.js
+++ server/routes/api.js
@@ -15,16 +15,16 @@ router.get('/dashboard/index', function (req, res) {
 router.get('/order', function (req, res) {
     Orders.find({}, function (err, rows) {
         if (err)
-            res.send(err);
-        res.send(rows);
+            res.json(err);
+        res.json(rows);
     });
 });
 
 router.get('/order/:id', function (req, res) {
     Orders.findOne({_id: req.params.id}, function (err, order) {
         if (err)
-            res.send(err);
-        res.send(order);
+            res.json(err);
+        res.json(order);
 
     });
 });
@@ -32,16 +32,16 @@ router.get('/order/:id', function (req, res) {
 router.get('/item', function (req, res) {
     Items.find({}, function (err, rows) {
         if (err)
-            res.send(err);
-        res.send(rows);
+            res.json(err);
+        res.json(rows);
     });
 });
 
 router.get('/item/:id', function (req, res) {
     Items.findOne({_id: req.params.id}, function (err, item) {
         if (err)
-            res.send(err);
-        res.send(item);
+            res.json(err);
+        res.json(item);
 
     });
 });
@@ -55,7 +55,10 @@ router.put('/item/:id', function (req, res) {
         },
         {upsert: true},
         function (err, obj) {
-            return res.json(true);
+            if (err) {
+                res.json(err);
+            }
+            res.json(obj);
         });
 });
 
@@ -66,10 +69,18 @@ router.post('/item', function (req, res) {
     item.price = req.body.price;
     item.save(function (err) {
         if (err) {
-            console.log(err);
-            res.send(err);
+            res.json(err);
         }
         res.json({message: 'Bear created!'});
+    });
+});
+
+router.delete('/item/:id', function (req, res) {
+    Items.findByIdAndRemove(req.params.id, function (err, response) {
+        if (err) {
+            res.json(err);
+        }
+        res.json({message: 'Deleted!'});
     });
 });
 
